2f6a16f7f2483df52fe3efb0d280b1cd0956f030
Carl Alldis <carlalldis@gmail.com>
2021-05-08 22:56:47 +0100

    Initial commit

---
+++ XboxLegoController/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SharpBrick.PoweredUp;
+using SharpBrick.PoweredUp.Functions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace XboxLegoController
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection()
+                .AddLogging(configure => configure.AddConsole())
+                .AddPoweredUp()
+                .AddWinRTBluetooth(); // using WinRT Bluetooth on Windows (separate NuGet SharpBrick.PoweredUp.WinRT; others are available)
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var host = serviceProvider.GetService<PoweredUpHost>();
+
+            var hub = await host.DiscoverAsync<TechnicMediumHub>();
+            await hub.ConnectAsync();
+
+            var Car = new TopGearRallyCar(hub);
+            await Car.Initialize();
+
+            Console.WriteLine("Speed 100");
+            await Car.SetDriveSpeed(10);
+            Console.ReadLine();
+            Console.WriteLine("Speed -100");
+            await Car.SetDriveSpeed(-10);
+            Console.ReadLine();
+            Console.WriteLine("Speed 0");
+            await Car.SetDriveSpeed(0);
+            Console.ReadLine();
+            await hub.SwitchOffAsync();
+        }
+
+        private static async Task<string> GetInputAsync()
+        {
+            return await Task.Run(() => Console.ReadLine());
+        }
+    }
+}
---
+++ XboxLegoController/TopGearRallyCar.cs
@@ -0,0 +1,45 @@
+﻿using SharpBrick.PoweredUp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XboxLegoController
+{
+    class TopGearRallyCar
+    {
+        private TechnicMediumHub Hub;
+        private TechnicLargeLinearMotor SteeringMotor;
+        private TechnicXLargeLinearMotor DriveMotor;
+
+        public TopGearRallyCar(TechnicMediumHub hub)
+        {
+            Hub = hub;
+        }
+
+        public async Task Initialize()
+        {
+            await Hub.RgbLight.SetRgbColorsAsync(0xff, 0x00, 0x00);
+            await Hub.VerifyDeploymentModelAsync(modelBuilder => modelBuilder
+                .AddHub<TechnicMediumHub>(hubBuilder => hubBuilder
+                    .AddDevice<TechnicLargeLinearMotor>(Hub.B)
+                    .AddDevice<TechnicXLargeLinearMotor>(Hub.D)
+                )
+            );
+            SteeringMotor = Hub.B.GetDevice<TechnicLargeLinearMotor>();
+            DriveMotor = Hub.D.GetDevice<TechnicXLargeLinearMotor>();
+            await Hub.RgbLight.SetRgbColorsAsync(0x00, 0xff, 0x00);
+        }
+
+        public async Task SetSteeringDegrees(int degrees)
+        {
+            await SteeringMotor.GotoPositionAsync(degrees, 10, 100, SpecialSpeed.Brake);
+        }
+
+        public async Task SetDriveSpeed(int speed)
+        {
+            await DriveMotor.StartSpeedAsync((sbyte)speed, 100, SpeedProfiles.None);
+        }
+    }
+}
